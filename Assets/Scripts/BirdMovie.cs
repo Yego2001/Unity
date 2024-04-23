d5c05bb07d3550598e935805c52c5ac7cd064802
egor.lobov <egorka_lobov_2013@outlook.com>
2024-04-23 22:12:47 +0300

    23.04.24

--- Assets/Scripts/BirdMovie.cs
+++ Assets/Scripts/BirdMovie.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class BirdMovie : MonoBehaviour
 {
     private Vector3 startPosition;
-    private float tapForce = 11;
+    private float tapForce = 501;
     private Rigidbody2D rb;
+    public float speed = 0;
+    public float rotationSpeed;
+    public float maxRotationZ;
+    public float minRotationZ;
+    private Quaternion maxRotation;
+    private Quaternion minRotation;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        startPosition = transform.position;
+        maxRotation = Quaternion.Euler(0, 0, maxRotationZ);
+        minRotation = Quaternion.Euler(0, 0, minRotationZ);
+
 
     }
 
@@ -20,6 +32,10 @@ public class BirdMovie : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector3.up * tapForce, ForceMode2D.Force);
+            rb.velocity = Vector3.right * speed;
+            transform.rotation = maxRotation;
         }
+        //transform.rotation = Quaternion.Lerp(transform.rotation, minRotation, Time.deltaTime * rotationSpeed);
+
     }
 }
