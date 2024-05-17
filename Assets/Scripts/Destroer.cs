1750d98bc1e699e59d6ec0abd694f422b9263d14
egor.lobov <egorka_lobov_2013@outlook.com>
2024-05-17 22:18:14 +0300

    17.05.24

--- Assets/Scripts/Destroer.cs
+++ Assets/Scripts/Destroer.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
+
 
 public class Destroer : MonoBehaviour
 {
     private Transform mainCamera;
+
+
+    // public Transform pipes;
     void Start()
     {
         mainCamera = GameObject.Find("Main Camera").GetComponent<Transform>();
+
+
+
     }
 
     // Update is called once per frame
-    void Update()
+    void Update(
+    )
     {
+        Transform pipe = this.gameObject.GetComponent<Transform>();
+
+        if ((mainCamera.position.x - pipe.position.x) > 10)
+        {
+            Destroy(this.gameObject);
+        }
+
+
 
     }
 }
