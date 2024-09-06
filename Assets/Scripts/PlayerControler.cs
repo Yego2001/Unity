80d3b12595c7c36803c5ee1f2876e65280205191
egor.lobov <egorka_lobov_2013@outlook.com>
2024-09-06 18:24:34 +0300

    06.09.24

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -6,8 +7,9 @@ using UnityEngine;
 public class PlayerControler : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float moveSpeed = 20;
-    public float jumpSpeed = 10;
+    public float moveSpeed = 1000;
+    public float jumpSpeed = 333;
+
 
     void Start()
     {
@@ -19,10 +21,22 @@ public class PlayerControler : MonoBehaviour
     {
 
         float horisontalInput = Input.GetAxis("Horizontal");
-        rb.AddForce(Vector3.right * moveSpeed * horisontalInput * Time.deltaTime);
-        if (Input.GetAxis(KeyCode.Space))
+        // float verticalInput = Input.GetAxis("Vertical");
+
+
+        rb.AddForce(Vector3.right * moveSpeed * horisontalInput * Time.deltaTime, ForceMode2D.Force);
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(Vector3.up * jumpSpeed * Time.deltaTime)
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode2D.Force);
+
         }
+
+
+        // rb.AddForce(Vector2.right * moveSpeed * horisontalInput * Time.deltaTime);
+        // if (Input.GetAxis("Horizontal") > 0)
+        // {
+        //     rb.AddForce(Vector3.up * jumpSpeed * Time.deltaTime);
+        // }
     }
 }
