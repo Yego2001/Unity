1128da11cb949506e9222d65497e56c891756619
egor.lobov <egorka_lobov_2013@outlook.com>
2024-10-12 10:17:24 +0300

    10.10.24

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,6 +7,7 @@ public class CameraFollow : MonoBehaviour
     public Transform player;
     public Vector3 offset;
     public float smouthTime;
+    private Vector3 point;
     void Start()
     {
 
@@ -21,8 +22,17 @@ public class CameraFollow : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 pointPosition = Vector3.Lerp(transform.position, player.position, smouthTime);
-        transform.position = pointPosition + offset;
+        FollowMe();
+
+    }
+
+
+
+    private void FollowMe()
+    {
+        point.x = player.position.x;
+        Vector3 pointPosition = Vector3.Lerp(transform.position, point, smouthTime * Time.deltaTime);
+        transform.position = pointPosition;
 
     }
 }
