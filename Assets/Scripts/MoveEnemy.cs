0fca78b1097a61b185d5d51358214b7d67bfae7d
egor.lobov <egorka_lobov_2013@outlook.com>
2024-11-09 23:38:00 +0300

    09.11.24

--- Assets/Scripts/MoveEnemy.cs
+++ Assets/Scripts/MoveEnemy.cs
@@ -5,25 +5,27 @@ using UnityEngine;
 
 public class MoveEnemy : MonoBehaviour
 {
-    public float XleftMove;
-    public float XrightMove;
-    private float maxRight;
-    private float maxLeft;
+    public float maxRight;
+    public float maxLeft;
     public float enemySpeed = 5;
+    public int chaingeDirection = 1;
     void Start()
     {
-        maxRight = transform.position.x + XrightMove;
-        maxLeft = transform.position.x + XleftMove;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        int chaingeDirection = 1;
-        transform.Translate(Vector3.right * chaingeDirection);
-        if (transform.position.x < maxLeft || transform.position.x > maxRight)
+        transform.Translate(Vector3.right * chaingeDirection * enemySpeed * Time.deltaTime);
+        if (transform.position.x > maxLeft)
         {
-            chaingeDirection *= -1;
+            chaingeDirection = -1;
+            transform.position = new Vector3(transform.position.x - 0.5f, transform.position.z);
+        }
+        if (transform.position.x < maxRight)
+        {
+            chaingeDirection = 1;
         }
 
 
