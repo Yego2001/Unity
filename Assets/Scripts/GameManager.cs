54c93bcd175cc1b6d05f4c2d361f244088f5aa65
egor.lobov <egorka_lobov_2013@outlook.com>
2025-02-12 22:57:43 +0300

    11.02.2025

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     private float balance;
     public TextMeshProUGUI balanceText;
-    private GameObject canvas;
+    public Button buttonComponent;
+
+
 
     void Start()
     {
 
 
-        balanceText.SetText("0");
+
 
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -27,5 +31,11 @@ public class GameManager : MonoBehaviour
     private void ButtonClick(int ScoreToAdd)
     {
         balance += ScoreToAdd;
+    }
+
+
+    public void updateText()
+    {
+        balanceText.SetText(balance + "");
     }
 }
